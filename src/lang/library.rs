4d4e76f51d045b12160266feba730e090bef674e
5eqn <subcat2077@gmail.com>
2024-04-19 16:39:52 +0800

    feat: failed function refact

--- src/lang/library.rs
+++ src/lang/library.rs
@@ -1,4 +1,5 @@
 use std::fmt::Display;
+use super::*;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Lib {
@@ -23,6 +24,45 @@ impl Display for Lib {
             Lib::Le => write!(f, "le"),
             Lib::Gt => write!(f, "gt"),
             Lib::Ge => write!(f, "ge"),
+        }
+    }
+}
+
+impl From<Lib> for Value {
+    fn from(lib: Lib) -> Self {
+        match lib {
+            Lib::Add => {
+                let f: Box<FF2F> = Box::new(|x, y| x + y);
+                f.into()
+            },
+            Lib::Sub => {
+                let f: Box<FF2F> = Box::new(|x, y| x - y);
+                f.into()
+            },
+            Lib::Mul => {
+                let f: Box<FF2F> = Box::new(|x, y| x * y);
+                f.into()
+            },
+            Lib::Div => {
+                let f: Box<FF2F> = Box::new(|x, y| x / y);
+                f.into()
+            },
+            Lib::Lt => {
+                let f: Box<FF2B> = Box::new(|x, y| x < y);
+                f.into()
+            },
+            Lib::Le => {
+                let f: Box<FF2B> = Box::new(|x, y| x <= y);
+                f.into()
+            },
+            Lib::Gt => {
+                let f: Box<FF2B> = Box::new(|x, y| x > y);
+                f.into()
+            },
+            Lib::Ge => {
+                let f: Box<FF2B> = Box::new(|x, y| x >= y);
+                f.into()
+            }
         }
     }
 }
